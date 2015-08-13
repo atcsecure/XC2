1c7bed7492799e26d1957391e8a94a857a9741c4
corvaxx <maybe.mail@inbox.ru>
2015-08-13 12:28:09 +0300

    small fix

--- src/qt/xbridge/xbridgeaddressbookview.cpp
+++ src/qt/xbridge/xbridgeaddressbookview.cpp
@@ -35,24 +35,21 @@ void XBridgeAddressBookView::onAddressSelect(QModelIndex index)
 void XBridgeAddressBookView::setupUi()
 {
     m_entryList = new QTableView(this);
+    m_entryList->setMinimumWidth(600);
     m_entryList->setModel(&m_model);
     m_entryList->setSelectionBehavior(QAbstractItemView::SelectRows);
 
     VERIFY(connect(m_entryList, SIGNAL(doubleClicked(QModelIndex)),
                    this,        SLOT(onAddressSelect(QModelIndex))));
 
-//    QHeaderView * header = m_entryList->horizontalHeader();
-//#if QT_VERSION <0x050000
-//    header->setResizeMode(XBridgeAddressBookModel::Currency, QHeaderView::Stretch);
-//#else
-//    header->setSectionResizeMode(XBridgeAddressBookModel::Currency, QHeaderView::Stretch);
-//#endif
-//    header->resizeSection(XBridgeAddressBookModel::Name,  80);
-//#if QT_VERSION <0x050000
-//    header->setResizeMode(XBridgeAddressBookModel::Address, QHeaderView::Stretch);
-//#else
-//    header->setSectionResizeMode(XBridgeAddressBookModel::Address, QHeaderView::Stretch);
-//#endif
+    QHeaderView * header = m_entryList->horizontalHeader();
+    header->resizeSection(XBridgeAddressBookModel::Currency,  80);
+    header->resizeSection(XBridgeAddressBookModel::Name,  200);
+#if QT_VERSION <0x050000
+    header->setResizeMode(XBridgeAddressBookModel::Address, QHeaderView::Stretch);
+#else
+    header->setSectionResizeMode(XBridgeAddressBookModel::Address, QHeaderView::Stretch);
+#endif
 
     QHBoxLayout * hbox = new QHBoxLayout;
     hbox->addWidget(m_entryList);
